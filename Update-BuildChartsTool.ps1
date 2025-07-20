17eeaa81fe909d84ad27da20fc060138642e3871
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-07-20 17:49:08 +0200

    Add Update-BuildChartsTool.ps1

---
+++ Update-BuildChartsTool.ps1
@@ -0,0 +1,5 @@
+dotnet tool install --global dotnet-buildcharts --add-source ./.buildcharts/output/nuget
+buildcharts generate
+dotnet tool uninstall --global dotnet-buildcharts
+$env:VERSION="1.0.0"; $env:COMMIT="abc123"; docker buildx bake --file buildcharts.hcl
+dotnet tool install --global dotnet-buildcharts --add-source ./.buildcharts/output/nuget
