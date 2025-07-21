251e49e33cb4e8a61c5de7c7e92fec0e1d491bef
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-07-21 02:56:41 +0200

    Add validation for build.yml

--- src/BuildCharts.Tool/Commands/GenerateCommand.cs
+++ src/BuildCharts.Tool/Commands/GenerateCommand.cs
@@ -25,24 +25,23 @@ public class GenerateCommand
         {
             var (_, buildConfig) = await ConfigurationManager.ReadBuildConfigAsync(ct);
             var (_, chartConfig) = await ConfigurationManager.ReadChartConfigAsync(ct);
+
+            _dockerHclGenerator.Validate(buildConfig);
             
             var plugins = PluginManager.LoadPlugins(buildConfig.Plugins);
-            foreach (var plugin in plugins)
-            {
-                await plugin.OnBeforeGenerateAsync(buildConfig, ct);
-            }
 
             Console.WriteLine("Pulling charts...");
             var pullTasks = chartConfig.Dependencies.Select(dependency => OrasClient.Pull($"{dependency.Repository}/{dependency.Name}:{dependency.Version}"));
             await Task.WhenAll(pullTasks);
 
+            var onBeforeGeneratePluginTasks = plugins.Select(x => x.OnBeforeGenerateAsync(buildConfig, ct));
+            await Task.WhenAll(onBeforeGeneratePluginTasks);
+
             var hclStringBuilder = await _dockerHclGenerator.GenerateAsync(buildConfig, chartConfig, UseInlineDockerFile);
-
-            foreach (var plugin in plugins)
-            {
-                await plugin.OnAfterGenerateAsync(buildConfig, chartConfig, hclStringBuilder, ct);
-            }
-
+           
+            var onAfterGeneratePluginTasks = plugins.Select(x => x.OnAfterGenerateAsync(buildConfig, chartConfig, hclStringBuilder, ct));
+            await Task.WhenAll(onAfterGeneratePluginTasks);
+            
             await File.WriteAllTextAsync("buildcharts.hcl", hclStringBuilder.ToString(), ct);
 
             Console.WriteLine("");
