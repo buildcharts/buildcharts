1dfcc3dc783e10e1f9316637d1cd4040836d068e
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-11-09 01:32:41 +0100

    Process plugins sequentially

--- src/BuildCharts.Tool/Commands/GenerateCommand.cs
+++ src/BuildCharts.Tool/Commands/GenerateCommand.cs
@@ -34,13 +34,17 @@ public class GenerateCommand
             var pullTasks = chartConfig.Dependencies.Select(dependency => OrasClient.Pull($"{dependency.Repository}/{dependency.Name}:{dependency.Version}"));
             await Task.WhenAll(pullTasks);
 
-            var onBeforeGeneratePluginTasks = plugins.Select(x => x.OnBeforeGenerateAsync(buildConfig, ct));
-            await Task.WhenAll(onBeforeGeneratePluginTasks);
-
+            foreach (var plugin in plugins)
+            {
+                await plugin.OnBeforeGenerateAsync(buildConfig, ct);
+            }
+          
             var hclStringBuilder = await _dockerHclGenerator.GenerateAsync(buildConfig, chartConfig, UseInlineDockerFile);
-           
-            var onAfterGeneratePluginTasks = plugins.Select(x => x.OnAfterGenerateAsync(buildConfig, chartConfig, hclStringBuilder, ct));
-            await Task.WhenAll(onAfterGeneratePluginTasks);
+
+            foreach (var plugin in plugins)
+            {
+                await plugin.OnAfterGenerateAsync(buildConfig, chartConfig, hclStringBuilder, ct);
+            }
             
             await File.WriteAllTextAsync(Path.Join(".buildcharts", "docker-bake.hcl"), hclStringBuilder.ToString(), ct);
 
