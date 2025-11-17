da8aa99198d47af21394a4567726ab0841fcf8d5
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-11-18 00:40:53 +0100

    Fix Update-BuildChartsTool.ps1 after switching to local builds

--- Update-BuildChartsTool.ps1
+++ Update-BuildChartsTool.ps1
@@ -1,5 +1,4 @@
-dotnet tool install --global dotnet-buildcharts --add-source ./.buildcharts/output/nuget
-buildcharts generate
+Remove-Item .buildcharts/output/nuget -Recurse -Force -ErrorAction SilentlyContinue
 dotnet tool uninstall --global dotnet-buildcharts
-$env:VERSION="1.0.0"; $env:COMMIT="abc123"; docker buildx bake --file .buildcharts/docker-bake.hcl
+$env:VERSION="1.0.0"; $env:COMMIT=(& git rev-parse HEAD); docker buildx bake --file .buildcharts/docker-bake.hcl --no-cache
 dotnet tool install --global dotnet-buildcharts --add-source ./.buildcharts/output/nuget
