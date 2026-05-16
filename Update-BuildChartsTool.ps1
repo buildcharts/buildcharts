eb2a3188bcd3319addcc8fdfa12650353040ae7c
Eddie Tisma <eddietisma@users.noreply.github.com>
2026-05-16 02:33:20 +0200

    Fix Update-BuildchartsTool.ps1 to only run nuget target

--- Update-BuildChartsTool.ps1
+++ Update-BuildChartsTool.ps1
@@ -1,4 +1,4 @@
 Remove-Item .buildcharts/output/nuget -Recurse -Force -ErrorAction SilentlyContinue
 dotnet tool uninstall --global dotnet-buildcharts
-$env:VERSION="1.0.0"; $env:COMMIT=(& git rev-parse HEAD); docker buildx bake --file .buildcharts/docker-bake.hcl --no-cache
+$env:VERSION="1.0.0"; $env:COMMIT=(& git rev-parse HEAD); docker buildx bake --file .buildcharts/docker-bake.hcl nuget --no-cache
 dotnet tool install --global dotnet-buildcharts --add-source ./.buildcharts/output/nuget
